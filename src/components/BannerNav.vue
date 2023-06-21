103f0a47c6d462a212b87a35f3f40007d6775877
gyamficodes <gyamfijohn08@gmail.com>
2023-06-21 10:03:40 -0700

    nav completed

--- src/components/BannerNav.vue
+++ src/components/BannerNav.vue
@@ -15,7 +15,15 @@
                 <div class="sub-menu1">
                 <ul>
                   <li><a href="#">company</a></li>
-                  <li><a href="#">service</a></li>
+                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                  <div class="sub-menu2">
+                  <ul>
+                    <li><a href="#">Developers</a></li>
+                  <li><a href="#">UI/UX</a></li>
+                  <li><a href="#">Bitcoin</a></li>
+                  </ul>
+                  </div>
+                  </li>
                   <li><a href="#">it solution</a></li>
                   <li><a href="#">element</a></li>
                   <li><a href="#">blog</a></li>
@@ -23,9 +31,66 @@
                 </ul>
                 </div>
                 </li>
-                <li><a href="#">company</a><i class="fa-solid fa-chevron-down"></i></li>
-                <li><a href="#">service</a></li>
-                <li><a href="#">it solution</a><i class="fa-solid fa-chevron-down"></i></li>
+                <li><a href="#">company</a><i class="fa-solid fa-chevron-down"></i>
+                  <div class="sub-menu1">
+                <ul>
+                  <li><a href="#">home</a></li>
+                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                  <!-- <div class="sub-menu2">
+                  <ul>
+                    <li><a href="#">Developers</a></li>
+                  <li><a href="#">UI/UX</a></li>
+                  <li><a href="#">Bitcoin</a></li>
+                  </ul>
+                  </div> -->
+                  </li>
+                  <li><a href="#">it solution</a></li>
+                  <li><a href="#">element</a></li>
+                  <li><a href="#">blog</a></li>
+                  <li><a href="#">contact</a></li>
+                </ul>
+                </div>
+                </li>
+                <li><a href="#">service</a>
+                  <div class="sub-menu1">
+                <ul>
+                  <li><a href="#">home</a></li>
+                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                  <div class="sub-menu2">
+                  <ul>
+                    <li><a href="#">Developers</a></li>
+                  <li><a href="#">UI/UX</a></li>
+                  <li><a href="#">Bitcoin</a></li>
+                  </ul>
+                  </div>
+                  </li>
+                  <li><a href="#">company</a></li>
+                  <li><a href="#">element</a></li>
+                  <li><a href="#">blog</a></li>
+                  <li><a href="#">contact</a></li>
+                </ul>
+                </div>
+                </li>
+                <li><a href="#">it solution</a><i class="fa-solid fa-chevron-down"></i>
+                  <div class="sub-menu1">
+                <ul>
+                  <li><a href="#">industries</a></li>
+                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                  <div class="sub-menu2">
+                  <ul>
+                    <li><a href="#">Designers</a></li>
+                  <li><a href="#">UI/UX</a></li>
+                  <li><a href="#">Bitcoin</a></li>
+                  </ul>
+                  </div>
+                  </li>
+                  <li><a href="#">it service</a></li>
+                  <li><a href="#">product</a></li>
+                  <li><a href="#">designs</a></li>
+                  <li><a href="#">details</a></li>
+                </ul>
+                </div>
+                </li>
                 <li><a href="#">element</a><i class="fa-solid fa-chevron-down"></i></li>
                 <li><a href="#">blog</a><i class="fa-solid fa-chevron-down"></i></li>
                 <li><a href="#">contact</a></li>
