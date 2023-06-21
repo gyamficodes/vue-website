98bf0cc4e6554b2795c7932df78caa3872084bc2
gyamficodes <gyamfijohn08@gmail.com>
2023-06-21 01:13:06 -0700

    navbar update

--- src/components/BannerNav.vue
+++ src/components/BannerNav.vue
@@ -9,8 +9,31 @@
         </div>
         <div class="nav-right">
           <div class="right-box">
-            <div class=""></div>
-            <div class=""></div>
+            <div class="nav-links">
+              <ul>
+                <li><a href="#">home</a><i class="fa-solid fa-chevron-down"></i>
+                <div class="sub-menu1">
+                <ul>
+                  <li><a href="#">company</a></li>
+                  <li><a href="#">service</a></li>
+                  <li><a href="#">it solution</a></li>
+                  <li><a href="#">element</a></li>
+                  <li><a href="#">blog</a></li>
+                  <li><a href="#">contact</a></li>
+                </ul>
+                </div>
+                </li>
+                <li><a href="#">company</a><i class="fa-solid fa-chevron-down"></i></li>
+                <li><a href="#">service</a></li>
+                <li><a href="#">it solution</a><i class="fa-solid fa-chevron-down"></i></li>
+                <li><a href="#">element</a><i class="fa-solid fa-chevron-down"></i></li>
+                <li><a href="#">blog</a><i class="fa-solid fa-chevron-down"></i></li>
+                <li><a href="#">contact</a></li>
+              </ul>
+            </div>
+            <div class="nav-btn">
+            <span>get a quote</span>
+            </div>
           </div>
         </div>
       </div>
