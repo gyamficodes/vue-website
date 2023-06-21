fc1ad3117d82adfc8b5031d043d2721f8891a540
gyamficodes <gyamfijohn08@gmail.com>
2023-06-21 11:58:56 -0700

    banner sec finished.

--- src/components/BannerNav.vue
+++ src/components/BannerNav.vue
@@ -12,84 +12,84 @@
             <div class="nav-links">
               <ul>
                 <li><a href="#">home</a><i class="fa-solid fa-chevron-down"></i>
-                <div class="sub-menu1">
-                <ul>
-                  <li><a href="#">company</a></li>
-                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
-                  <div class="sub-menu2">
-                  <ul>
-                    <li><a href="#">Developers</a></li>
-                  <li><a href="#">UI/UX</a></li>
-                  <li><a href="#">Bitcoin</a></li>
-                  </ul>
+                  <div class="sub-menu1">
+                    <ul>
+                      <li><a href="#">company</a></li>
+                      <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                        <div class="sub-menu2">
+                          <ul>
+                            <li><a href="#">Developers</a></li>
+                            <li><a href="#">UI/UX</a></li>
+                            <li><a href="#">Bitcoin</a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li><a href="#">it solution</a></li>
+                      <li><a href="#">element</a></li>
+                      <li><a href="#">blog</a></li>
+                      <li><a href="#">contact</a></li>
+                    </ul>
                   </div>
-                  </li>
-                  <li><a href="#">it solution</a></li>
-                  <li><a href="#">element</a></li>
-                  <li><a href="#">blog</a></li>
-                  <li><a href="#">contact</a></li>
-                </ul>
-                </div>
                 </li>
                 <li><a href="#">company</a><i class="fa-solid fa-chevron-down"></i>
                   <div class="sub-menu1">
-                <ul>
-                  <li><a href="#">home</a></li>
-                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
-                  <!-- <div class="sub-menu2">
+                    <ul>
+                      <li><a href="#">home</a></li>
+                      <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                        <!-- <div class="sub-menu2">
                   <ul>
                     <li><a href="#">Developers</a></li>
                   <li><a href="#">UI/UX</a></li>
                   <li><a href="#">Bitcoin</a></li>
                   </ul>
                   </div> -->
-                  </li>
-                  <li><a href="#">it solution</a></li>
-                  <li><a href="#">element</a></li>
-                  <li><a href="#">blog</a></li>
-                  <li><a href="#">contact</a></li>
-                </ul>
-                </div>
+                      </li>
+                      <li><a href="#">it solution</a></li>
+                      <li><a href="#">element</a></li>
+                      <li><a href="#">blog</a></li>
+                      <li><a href="#">contact</a></li>
+                    </ul>
+                  </div>
                 </li>
                 <li><a href="#">service</a>
                   <div class="sub-menu1">
-                <ul>
-                  <li><a href="#">home</a></li>
-                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
-                  <div class="sub-menu2">
-                  <ul>
-                    <li><a href="#">Developers</a></li>
-                  <li><a href="#">UI/UX</a></li>
-                  <li><a href="#">Bitcoin</a></li>
-                  </ul>
+                    <ul>
+                      <li><a href="#">home</a></li>
+                      <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                        <div class="sub-menu2">
+                          <ul>
+                            <li><a href="#">Developers</a></li>
+                            <li><a href="#">UI/UX</a></li>
+                            <li><a href="#">Bitcoin</a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li><a href="#">company</a></li>
+                      <li><a href="#">element</a></li>
+                      <li><a href="#">blog</a></li>
+                      <li><a href="#">contact</a></li>
+                    </ul>
                   </div>
-                  </li>
-                  <li><a href="#">company</a></li>
-                  <li><a href="#">element</a></li>
-                  <li><a href="#">blog</a></li>
-                  <li><a href="#">contact</a></li>
-                </ul>
-                </div>
                 </li>
                 <li><a href="#">it solution</a><i class="fa-solid fa-chevron-down"></i>
                   <div class="sub-menu1">
-                <ul>
-                  <li><a href="#">industries</a></li>
-                  <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
-                  <div class="sub-menu2">
-                  <ul>
-                    <li><a href="#">Designers</a></li>
-                  <li><a href="#">UI/UX</a></li>
-                  <li><a href="#">Bitcoin</a></li>
-                  </ul>
+                    <ul>
+                      <li><a href="#">industries</a></li>
+                      <li class="hover-me"><a href="#">service</a><i class="fa-solid fa-chevron-left"></i>
+                        <div class="sub-menu2">
+                          <ul>
+                            <li><a href="#">Designers</a></li>
+                            <li><a href="#">UI/UX</a></li>
+                            <li><a href="#">Bitcoin</a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li><a href="#">it service</a></li>
+                      <li><a href="#">product</a></li>
+                      <li><a href="#">designs</a></li>
+                      <li><a href="#">details</a></li>
+                    </ul>
                   </div>
-                  </li>
-                  <li><a href="#">it service</a></li>
-                  <li><a href="#">product</a></li>
-                  <li><a href="#">designs</a></li>
-                  <li><a href="#">details</a></li>
-                </ul>
-                </div>
                 </li>
                 <li><a href="#">element</a><i class="fa-solid fa-chevron-down"></i></li>
                 <li><a href="#">blog</a><i class="fa-solid fa-chevron-down"></i></li>
@@ -97,7 +97,7 @@
               </ul>
             </div>
             <div class="nav-btn">
-            <span>get a quote</span>
+              <span>get a quote</span>
             </div>
           </div>
         </div>
@@ -117,9 +117,9 @@ export default {
     };
   },
   methods: {},
-  mounted() {},
+  mounted() { },
   computed: {},
-  created() {},
+  created() { },
 };
 </script>
 
--- src/components/BannerSection.vue
+++ src/components/BannerSection.vue
@@ -1,30 +1,61 @@
 <template>
-   <section class="banner-page" :style="`background: url(${background});`">
-<BannerNav/>
-   </section>
+    <section class="banner-page" :style="`background-image: url(${background});`">
+        <BannerNav />
+        <section class="banner-content">
+            <div class="container">
+                <div class="content-menu">
+                    <div class="content-text">
+                        <h1>We Provide The Best
+                            <br> All Kind IT Solution
+                        </h1>
+                        <p>Morem upsum dolor set amet, consectetur adipisicing elit, sed do eiusmod tempor <br> incididunt
+                            ut
+                            laboret dolore magna aliqua.
+                        </p>
+                        <div class="box">
+
+                            <div class="nav-btn">
+                                <span>how it work <i class="fa-solid fa-arrow-right-long fa-beat"></i></span>
+                            </div>
+                            <div class="btn">
+                                <i class="fa-solid fa-play"></i></div>
+                        </div>
+                    </div>
+                    <div class="content-image">
+                        <img class="anima" :src="anima" alt="">
+                        <img class="pic" :src="shape" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
 </template>
 
 <script>
 import baner from '../assets/slider-3.png';
 import BannerNav from './BannerNav.vue';
+import round from '../assets/sdt.png';
+import pic from '../assets//shape1.png';
 
-    export default {
-        name:'BannerSection',
-        components:{
-            BannerNav
-        },
-        data(){
-            return{
-                background:baner,
-            }
-        },
-        methods:{},
-        mounted(){},
-        computed:{},
-        created(){},
-    }
+export default {
+    name: 'BannerSection',
+    components: {
+        BannerNav
+    },
+    data() {
+        return {
+            background: baner,
+            anima: round,
+            shape:  pic,
+        }
+    },
+    methods: {},
+    mounted() { },
+    computed: {},
+    created() { },
+}
 </script>
 
 <style lang="scss">
- @import'../assets/Sass/BannerSec';
+@import'../assets/Sass/BannerSec';
 </style>
