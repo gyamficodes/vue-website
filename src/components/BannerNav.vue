5b46405c3b90241e559bfc1fc9ecbc359a740af1
gyamficodes <gyamfijohn08@gmail.com>
2023-06-24 01:15:47 -0700

    src/servicePage updated

--- src/components/BannerNav.vue
+++ src/components/BannerNav.vue
@@ -9,25 +9,89 @@
         </div>
         <div class="nav-right">
           <div class="right-box">
-              <ul>
-                <li><a href="#">home</a><i class="fa-sharp fa-solid fa-chevron-down"></i>
+            <ul>
+              <li><a href="#">home</a><i class="fa-sharp fa-solid fa-chevron-down"></i>
                 <div class="sub-menu">
-                <ul>
-                  <li><a href="#">Company</a></li>
-                  <li><a href="#">Service </a><i class="fa-sharp fa-solid fa-chevron-left"></i></li>
-                  <li><a href="#">it solutions</a></li>
-                  <li><a href="#">element</a></li>
-                <li><a href="#">contact</a></li>
-                </ul>
+                  <ul>
+                    <li><a href="#">Company</a></li>
+                    <li><a href="#">Service </a><i class="fa-sharp fa-solid fa-chevron-left"></i>
+                      <div class="sub-menu1">
+                        <ul>
+                          <li><a href="#">it solutions</a></li>
+                          <li><a href="#">element</a></li>
+                          <li><a href="#">contact</a></li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li><a href="#">it solutions</a></li>
+                    <li><a href="#">element</a></li>
+                    <li><a href="#">contact</a></li>
+                  </ul>
                 </div>
-                </li>
-                <li><a href="#">company</a><i class="fa-sharp fa-solid fa-chevron-down"></i></li>
-                <li><a href="#">service</a></li>
-                <li><a href="#">it solutions</a><i class="fa-sharp fa-solid fa-chevron-down"></i></li>
-                <li><a href="#">element</a><i class="fa-sharp fa-solid fa-chevron-down"></i></li>
-                <li><a href="#">blog</a><i class="fa-sharp fa-solid fa-chevron-down"></i></li>
-              <li><a href="#">contact</a></li>
-              </ul>
+              </li>
+              <li><a href="#">company</a><i class="fa-sharp fa-solid fa-chevron-down"></i>
+                <div class="sub-menu">
+                  <ul>
+                    <li class="tt"><a href="#">home</a></li>
+                    <li><a href="#">Service </a><i class="fa-sharp fa-solid fa-chevron-left"></i>
+                      <div class="sub-menu1">
+                        <ul>
+                          <li><a href="#">web designer</a></li>
+                          <li><a href="#">developer</a></li>
+                          <li><a href="#"> manager</a></li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li><a href="#">it solutions</a></li>
+                    <li><a href="#">element</a></li>
+                    <li><a href="#">contact</a></li>
+                  </ul>
+                </div>
+
+              </li>
+              <li><a href="#">service</a>
+                <div class="sub-menu">
+                  <ul>
+                    <li class="tt"><a href="#">home</a></li>
+                    <li><a href="#">company </a><i class="fa-sharp fa-solid fa-chevron-left"></i>
+                      <div class="sub-menu1">
+                        <ul>
+                          <li><a href="#">web designer</a></li>
+                          <li><a href="#">developer</a></li>
+                          <li><a href="#"> manager</a></li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li><a href="#">it solutions</a></li>
+                    <li><a href="#">element</a></li>
+                    <li><a href="#">contact</a></li>
+                  </ul>
+                </div>
+
+              </li>
+              <li><a href="#">it solutions</a><i class="fa-sharp fa-solid fa-chevron-down"></i></li>
+              <li><a href="#">element</a><i class="fa-sharp fa-solid fa-chevron-down"></i>
+                <div class="sub-menu">
+                  <ul>
+                    <li class="tt"><a href="#">home</a></li>
+                    <li><a href="#">Service </a><i class="fa-sharp fa-solid fa-chevron-left"></i>
+                      <div class="sub-menu1">
+                        <ul>
+                          <li><a href="#">web designer</a></li>
+                          <li><a href="#">developer</a></li>
+                          <li><a href="#"> manager</a></li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li><a href="#">it solutions</a></li>
+                    <li><a href="#">element</a></li>
+                    <li><a href="#">contact</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li><a href="#">blog</a><i class="fa-sharp fa-solid fa-chevron-down"></i></li>
+              <li style="display: flex; align-items: center; color:#fff; gap:0.2em; cursor:pointer;"><a href="#">contact</a><span class="material-icons">phone</span></li>
+            </ul>
             <div class="nav-btn">
               <span>get a quote</span>
             </div>
--- src/components/ServicePage.vue
+++ src/components/ServicePage.vue
@@ -1,23 +1,62 @@
 <template>
-<section class="service-page-content"></section>
+  <section class="service-page-content">
+    <div class="container">
+      <div class="service-text">
+        <div class="text-card">
+          <div class="card" v-for="card in cards" :key="card.id">
+            <div class="num">
+              <span>{{ card.number }}</span>
+              <div class="wt"></div>
+            </div>
+            <div class="text">
+              <h3>{{ card.title }}</h3>
+              <p>{{ card.message }}</p>
+              <span>{{ card.details }} <i class="fa-solid fa-arrow-right"></i></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        name:'ServicePage',
-        components: {},
+export default {
+  name: 'ServicePage',
+  components: {},
   data() {
     return {
-
-    };
+      cards: [
+        {
+          id: 1,
+          number:"01",
+          title:'Marketing Strategy & Analysis',
+          message:'Whether bringing new amazing products and services to market discovering new ways to make mature',
+          details:'read more'
+        },
+        {
+          id: 2,
+          number:"02",
+          title:'Web Design & Development',
+          message:'Whether bringing new amazing products and services to market discovering new ways to make mature',
+          details:'read more'
+        },
+        {
+          id: 3,
+          number:"03",
+          title:'SEO & Digital Marketing',
+          message:'Whether bringing new amazing products and services to market discovering new ways to make mature',
+          details:'read more'
+        },
+        
+      ]
+    }
   },
   methods: {},
   mounted() { },
   computed: {},
   created() { },
-    }
+}
 </script>
 
-<style lang="scss">
-@import'../assets/Sass/ServicePage';
-</style>
+<style lang="scss">@import'../assets/Sass/ServicePage';</style>
