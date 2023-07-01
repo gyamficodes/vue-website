3db0c01b8e055bfa24bbe98b65a43861501d7d8d
gyamficodes <gyamfijohn08@gmail.com>
2023-06-30 20:13:23 -0700

    update

--- src/components/ChooseUsPage.vue
+++ src/components/ChooseUsPage.vue
@@ -7,7 +7,16 @@
           <h1>Provide Exclusive Services</h1>
           <div class="line"></div>
           <div class="card-page">
-          <div class="card" :style="`background: url(${pic1})`"></div>
+            <div class="card" :style="`background: url(${pic1})`">
+              <span class="material-icons">desktop_windows</span>
+              <h2>Deep Expertise & Leadership</h2>
+              <p>
+                We have the technology and industry expertise to develop
+                solutions that can connect people and businesses across a
+                variety of mobile devices.
+              </p>
+              <div class="btn">read more</div>
+            </div>
           </div>
         </div>
       </div>
@@ -25,11 +34,36 @@ export default {
   name: "ChooseUsPage",
   data() {
     return {
+      cards: [
+        {
+          id: 1,
+          pic: wh1,
+          btn: 'read more',
+          icon: "desktop_windows",
+          title: "Deep Expertise & Leadership",
+          text: "  We have the technology and industry expertise to develop solutions that can connect people and businesses across a  variety of mobile devices.",
+        },
+        {
+          id: 2,
+          pic: wh2,
+          btn: 'read more',
+          icon: "desktop_windows",
+          title: "Cross-Industry Expertise",
+          text: "  We have the technology and industry expertise to develop solutions that can connect people and businesses across a  variety of mobile devices.",
+        },
+        {
+          id: 3,
+          pic: wh3,
+          btn: 'read more',
+          icon: "desktop_windows",
+          title: "Dedicated IT Solution",
+          text: "  We have the technology and industry expertise to develop solutions that can connect people and businesses across a  variety of mobile devices.",
+        },
+      ],
       background: image,
-      pic1:wh1,
-      pic2:wh2,
-      pic3:wh3,
-
+      pic1: wh1,
+      pic2: wh2,
+      pic3: wh3,
     };
   },
   methods: {},
