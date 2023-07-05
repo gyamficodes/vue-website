08ba7cfb65a469cef2c11e16e760e9537f0fe62a
gyamficodes <gyamfijohn08@gmail.com>
2023-07-05 15:05:08 -0700

    contactPage ended

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -3,11 +3,36 @@
     <div class="contact-page-menu">
       <div class="contact">
         <ditv class="contactpage-top">
-        <h4>GET QUOTE</h4>
-        <h1>Make An <br> Free Consultant</h1>
-        <div class="line"></div>
+          <h4>GET QUOTE</h4>
+          <h1>
+            Make An <br />
+            Free Consultant
+          </h1>
+          <div class="line"></div>
         </ditv>
-      <ditv class="contactpage-down"></ditv>
+        <ditv class="contactpage-down">
+          <div class="input-page">
+            <input type="text" placeholder="name" required />
+            <input type="email" placeholder="email address" required />
+          </div>
+          <div class="input-page">
+            <input type="number" placeholder="phone number" required />
+            <input type="link" placeholder="website" required />
+          </div>
+
+          <div class="text-area">
+            <textarea
+              name=""
+              id=""
+              cols="147.9"
+              placeholder="write a message"
+            ></textarea>
+          </div>
+          <div class="btn-a">
+          free consultancy
+          </div>
+          <!--  -->
+        </ditv>
       </div>
     </div>
   </section>
