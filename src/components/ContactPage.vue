536e82465a0d4b73895dc70f6b0143c133321359
gyamficodes <gyamfijohn08@gmail.com>
2023-07-05 06:45:28 -0700

    update

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -1,8 +1,16 @@
 <template>
-  <section
-    class="ContactPage"
-    :style="`background-image: url(${pic})`"
-  ></section>
+  <section class="ContactPage" :style="`background-image: url(${pic})`">
+    <div class="contact-page-menu">
+      <div class="contact">
+        <ditv class="contactpage-top">
+        <h4>GET QUOTE</h4>
+        <h1>Make An <br> Free Consultant</h1>
+        <div class="line"></div>
+        </ditv>
+      <ditv class="contactpage-down"></ditv>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
