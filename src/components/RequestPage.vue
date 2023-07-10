06bcf7ebc483edc854f1c917c72d1e61ae0a85ed
gyamficodes <gyamfijohn08@gmail.com>
2023-07-09 17:33:16 -0700

    updates

--- src/components/RequestPage.vue
+++ src/components/RequestPage.vue
@@ -1,24 +1,37 @@
 <template>
-   <section class="RequesstPage" :style="`background-image: url(${image})1`"></section>
+  <section class="RequesstPage" :style="`background-image: url(${image})1`">
+    <div class="container">
+      <div class="Request-page-content">
+        <div class="Request-content">
+          <h4>+880 013 143 206</h4>
+          <h1>
+            To make requests for the <br />
+            further information
+          </h1>
+          <div class="btn">
+          <a href="#">join with now </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import pic from '../assets/call-bg.png';
-    export default {
-        data(){
-            return{
-                image:pic,
-            }
-        },
-        methods: {
-  
+import pic from "../assets/call-bg.png";
+export default {
+  data() {
+    return {
+      image: pic,
+    };
   },
+  methods: {},
   mounted() {},
   computed: {},
   created() {},
-    }
+};
 </script>
 
 <style lang="scss">
-@import'../assets/Sass/RequestPage';
+@import "../assets/Sass/RequestPage";
 </style>
