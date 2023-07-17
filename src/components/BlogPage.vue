c624fa615fab381bcbf6bff30b46360e6a09c977
gyamficodes <gyamfijohn08@gmail.com>
2023-07-17 15:16:42 -0700

    blogPage updated

--- src/components/BlogPage.vue
+++ src/components/BlogPage.vue
@@ -15,12 +15,21 @@
             <div class="line"></div>
           </div>
           <div class="downB">
-            <div class="card">
-              <div class="card-img" :style="`background: url(${blog})`"></div>
+            <div class="card" v-for="blog in  blogCards" :key="blog.id">
+              <div class="card-img" :style="`background: url(${blog.blog})`"></div>
               <div class="card-body">
                 <div class="date">
-                <a href="#"></a>
+                <a href="#">{{ blog.dateText }}</a>
+                <span></span>
+                <h5>{{ blog.date }}</h5>
                 </div>
+                <a class="title" href="#">{{ blog.title }}</a>
+                <p>{{ blog.myText }}</p>
+                <div class="box-text">
+                <h4>{{ blog.btn }}</h4>
+                <span class="material-icons">{{ blog.icon }}</span>
+                </div>
+                
               </div>
             </div>
           </div>
@@ -32,6 +41,8 @@
 
 <script>
 import blog1 from "../assets/blog1.jpg";
+import blog2 from "../assets/blog2.jpg";
+import blog3 from "../assets/blog3.jpg";
 
 export default {
   name: "BlogPage",
@@ -39,7 +50,40 @@ export default {
   props: [],
   data() {
     return {
-      blog: blog1,
+    
+
+      blogCards:[
+          {
+         id:1,
+         title:'The five devices you need to work anytime',
+         blog:blog1,
+         myText:'Lorem ipsum dolor sit amet consectet adipisie cing elit sed eiusmod tempor incididunt on labore et dolore.',
+         btn: 'read more',
+         icon:'arrow_right_alt',
+         dateText:'Techno',
+         date:'January 3, 2023',
+          },
+          {
+         id:1,
+         title:'How to learn PHP 10 tips to get you started',
+         blog:blog2,
+         myText:'Lorem ipsum dolor sit amet consectet adipisie cing elit sed eiusmod tempor incididunt on labore et dolore.',
+         btn: 'read more',
+         icon:'arrow_right_alt',
+         dateText:'Techno',
+         date:'December 3, 2023',
+          },
+          {
+         id:1,
+         title:'The five devices you need to work anytime',
+         blog:blog3,
+         myText:'Lorem ipsum dolor sit amet consectet adipisie cing elit sed eiusmod tempor incididunt on labore et dolore.',
+         btn: 'read more',
+         icon:'arrow_right_alt',
+         dateText:'Techno',
+         date:'Augost 5, 2023',
+          },
+      ],
       numbers: [
         {
           id: 1,
