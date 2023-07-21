67a023a4c3b185e4585ed12a7e91756c01bd5c4e
gyamficodes <gyamfijohn08@gmail.com>
2023-07-20 23:33:45 -0700

    update on my app

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -2,20 +2,23 @@
   <section class="footer_Page">
     <div class="container">
       <div class="Subscribe-Page">
-      <div class="sub-page-text">
-      <h2>Subscribe Our Newsletter</h2>
-      <div class="box-sub">
-        <input type="email" placeholder="ener your email" >
-        <div class="btnA">
-        <span>Subscribe</span>
+        <div class="sub-page-text">
+          <h2>Subscribe Our Newsletter</h2>
+          <div class="box-sub">
+            <input type="email" placeholder="ener your email" />
+            <div class="btnA">
+              <span>Subscribe</span>
+            </div>
+          </div>
         </div>
       </div>
+      <div class="sub-content-page">
+        <div>fesdgd</div>
+        <div>dsgfeht</div>
       </div>
-      
-      </div>
-      <div  class="sub-content-page">
-            <div>fesdgd</div>
-            <div>dsgfeht</div>
+
+      <div class="footer-nav">
+        <h1>dsgfdgd</h1>
       </div>
     </div>
   </section>
