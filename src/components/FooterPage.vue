7e2fcd155e67c3e6ba80ecfc810109e1c03e00dd
gyamficodes <gyamfijohn08@gmail.com>
2023-07-27 22:34:04 -0700

    update

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -13,7 +13,59 @@
         </div>
       </div>
       <div class="sub-content-page">
-        <div class="sub-footer-left">fesdgd</div>
+        <div class="sub-footer-left">
+          <div class="sub-footer-leftA">
+            <div class="forter-logo">
+              <img :src="logo" alt="" />
+            </div>
+            <div class="p-text">
+              <p>
+                Condimentum rhoncus, sem quam <br />
+                semper libero, sit amet adipiscing <br />
+                sem neque sed ipsum. Nam quam nunc, <br />
+                blandit vel, luctus.
+              </p>
+            </div>
+            <div class="fot-flex">
+              <span>Follow Us</span>
+              <div class="icons-flex">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                <a href="#"><i class="fa-brands fa-skype"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="sub-footer-leftB">
+            <div class="heading">
+              <h3>Our Services</h3>
+            </div>
+            <span
+              ><p></p>
+              Marketing Strategy
+            </span>
+            <span
+              ><p></p>
+              Interior Design
+            </span>
+            <span
+              ><p></p>
+              Digital Services
+            </span>
+            <span
+              ><p></p>
+              Product Selling
+            </span>
+            <span
+              ><p></p>
+              Product Design
+            </span>
+            <span
+              ><p></p>
+              Social Marketing
+            </span>
+          </div>
+        </div>
         <div class="sub-footer-right">dsgfeht</div>
       </div>
 
@@ -32,12 +84,16 @@
 </template>
 
 <script>
+import logo from "../assets/footre-logo.png";
+
 export default {
   name: "FooterPage",
   components: {},
   props: [],
   data() {
-    return {};
+    return {
+      logo,
+    };
   },
   methods: {},
   computed: {},
